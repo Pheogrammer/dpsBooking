d42f4ab788aabf19c6c8ef43e5426ff3c48591a1
Pheogrammer <ngelime07@gmail.com>
2023-09-08 10:57:34 +0300

    interfaced

--- resources/views/viewApplication.blade.php
+++ resources/views/viewApplication.blade.php
@@ -36,59 +36,80 @@
                             </div>
                         @endif
 
-                        <h4 class="mb-3">Personal Information</h4>
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
-                                <b for="name">Full Name<sup class='text-danger'>*</sup></b>
-                                {{ $data->name }}
-                            </div>
-                            <div class="col-md-6 mb-3">
-                                <b for="email">Email<sup class='text-danger'>*</sup></b>
-                                {{ $data->email }}
-                            </div>
-                        </div>
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td colspan="2">
+                                        <h4>Personal Information</h4>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b for="name">Full Name </b>
+                                        {{ $data->name }}
+                                    </td>
+                                    <td>
+                                        <b for="email">Email </b>
+                                        {{ $data->email }}
+                                    </td>
+                                </tr>
 
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
-                                <b for="phone">Phone Number<sup class='text-danger'>*</sup></b>
-                                {{ $data->phone }}
-                            </div>
-                            <div class="col-md-6 mb-3">
-                                <b for="address">Address</b>
-                                {{ $data->adress }}
-                            </div>
-                        </div>
+                                <tr>
+                                    <td>
+                                        <b for="phone">Phone Number </b>
+                                        {{ $data->phone }}
+                                    </td>
+                                    <td>
+                                        <b for="address">Address </b>
+                                        {{ $data->adress }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2"></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <h4>Booking Information</h4>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <b for="purpose">Purpose of Booking/Name of event</b>
+                                        {{ $data->purpose }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b for="entity">Name of the entity booking</b>
+                                        {{ $data->entity }}
+                                    </td>
+                                    <td>
+                                        <b for="participants">Approximately total participants</b>
+                                        {{ $data->number_of_participants }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+
+                                        <b> From:</b> {{ (new DateTime($data->start_date))->format('d/m/Y') }} - <b>To:</b>
+                                        {{ (new DateTime($data->end_date))->format('d/m/Y') }}
+
+                                    </td>
+                                    <td>
+                                        <b>Applied on</b>
+                                        {{ (new DateTime($data->created_at))->format('d/m/Y') }}
+
+                                    </td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+
+
 
                         <br>
-                        <h4 class="mb-3">Booking Information</h4>
 
-                        <div class="mb-3">
-                            <b for="purpose">Purpose of Booking/Name of event<sup class='text-danger'>*</sup></b>
-                            {{ $data->purpose }}
-                        </div>
 
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
-                                <b for="entity">Name of the entity booking<sup class='text-danger'>*</sup></b>
-                                {{ $data->entity }}
-                            </div>
-                            <div class="col-md-6 mb-3">
-                                <b for="participants">Approximately total participants<sup class='text-danger'>*</sup></b>
-                                {{ $data->number_of_participants }}
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col  ">
-                                <b> From:</b> {{ (new DateTime($data->start_date))->format('d/m/Y') }} - <b>To:</b>
-                                {{ (new DateTime($data->end_date))->format('d/m/Y') }}
-
-                                <br>
-                                <b>Applied on</b>
-                                {{ (new DateTime($data->created_at))->format('d/m/Y') }}
-
-                            </div>
-                        </div>
 
 
 
